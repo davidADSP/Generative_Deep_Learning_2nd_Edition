28af4384de6646c04243445840b124b5cf642f30
davidADSP <david@adsp.ai>
2022-04-20 06:31:46 +0000

    update GAN

--- utils/image.py
+++ utils/image.py
@@ -30,6 +30,11 @@ def display(images, n = 10, size = (20,3), cmap = 'gray_r', as_type = 'float32',
     """
     Displays n random images from each one of the supplied arrays.
     """
+    if images.max() > 1.0:
+        images = images / 255.0
+    elif images.min() < 0.0:
+        images = (images + 1.0) / 2.0
+
     plt.figure(figsize=size)
     for i in range(n):
         ax = plt.subplot(1, n, i + 1)
