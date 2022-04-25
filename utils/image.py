e5fa8a16b92a204bed59128116ba98680ff5b4bd
davidADSP <david@adsp.ai>
2022-04-25 09:48:30 +0000

    update gan

--- utils/image.py
+++ utils/image.py
@@ -3,28 +3,6 @@ import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
 
-def preprocess(imgs):
-    """
-    Normalize and reshape the images
-    """
-    imgs = imgs.astype("float32") / 255.0
-    imgs = np.pad(imgs , ((0,0), (2,2), (2,2)), constant_values= 0.0)
-    imgs = np.expand_dims(imgs, -1)
-    return imgs
-
-
-
-def noise(imgs, noise_factor):
-    """
-    Adds random noise to each image in the supplied array.
-    """
-    noisy_img = imgs + noise_factor * np.random.normal(
-        mean=0.0, stddev=1.0, shape=np.shape(imgs)
-    )
-
-    return np.clip_by_value(noisy_img, 0.0, 1.0)
-
-
 
 def display(images, n = 10, size = (20,3), cmap = 'gray_r', as_type = 'float32', save_to = None):
     """
