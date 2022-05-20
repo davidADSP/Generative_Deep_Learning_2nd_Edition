be95df4bd786a86833ecf1bdea657aec43b976e4
davidADSP <david@adsp.ai>
2022-05-20 20:09:27 +0000

    lstm model

--- scripts/tensorboard.sh
+++ scripts/tensorboard.sh
@@ -1,5 +1,4 @@
-CHAPTER=$1
-MODEL=$2
-DATASET=$3
-echo "Attaching Tensorboard to ./notebooks/$CHAPTER/$MODEL/$DATASET/logs/"
-docker-compose exec -e CHAPTER=$CHAPTER -e MODEL=$MODEL  -e DATASET=$DATASET app bash -c 'tensorboard --logdir "./notebooks/$CHAPTER/$MODEL/$DATASET/" --host 0.0.0.0 --port "$TENSORBOARD_PORT"'
+MODEL=$1
+DATASET=$2
+echo "Attaching Tensorboard to ./notebooks/$MODEL/$DATASET/logs/"
+docker-compose exec -e CHAPTER=$CHAPTER -e MODEL=$MODEL  -e DATASET=$DATASET app bash -c 'tensorboard --logdir "./notebooks/$MODEL/$DATASET/" --host 0.0.0.0 --port "$TENSORBOARD_PORT"'
