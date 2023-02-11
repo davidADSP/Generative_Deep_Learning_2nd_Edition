0ce42b9c6666143933fea28af6dace9a4b9c5bbd
davidADSP <david@adsp.ai>
2023-02-11 02:33:56 +0000

    auto

--- scripts/download_all_data.sh
+++ scripts/download_all_data.sh
@@ -1,2 +1,3 @@
 bash scripts/download_kaggle_data.sh jessicali9530 celeba-dataset
 bash scripts/download_kaggle_data.sh joosthazelzet lego-brick-images 
+bash scripts/download_kaggle_data.sh hugodarwood epirecipes
